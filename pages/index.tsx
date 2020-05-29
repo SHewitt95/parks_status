2e8b353486f378daf5c4b703c6fc24b27446178a
Sherman Hewitt <hewitt.sherman@gmail.com>
2020-05-29 09:59:27 -0400

    now we're cooking with data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,23 @@
-export default () => <p>{"I Live"}</p>
+import axios from 'axios';
+
+export default props => {
+  console.log({ props });
+  return (
+    <ul>
+      {props.data.map((item, idx) => (
+        <li key={idx}>{item.park_name}</li>
+      ))}
+    </ul>
+  );
+};
+
+const endpoint: string = 'https://www.nps.gov/nps-alerts.json?dt=159075838698';
+
+export async function getServerSideProps(context) {
+  const data = await axios.get(endpoint);
+  return {
+    props: {
+      data: data.data,
+    },
+  };
+}
