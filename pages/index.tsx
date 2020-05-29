695e693574d269a26153bb4a2d62cea594e7c027
Sherman Hewitt <hewitt.sherman@gmail.com>
2020-05-29 10:16:39 -0400

    start strict Typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import axios from 'axios';
+import Props from '../types/props';
 
-export default props => {
+export default (props: Props) => {
   console.log({ props });
   return (
     <ul>
-      {props.data.map((item, idx) => (
+      {props.data.map((item, idx: number) => (
         <li key={idx}>{item.park_name}</li>
       ))}
     </ul>
@@ -13,7 +14,7 @@ export default props => {
 
 const endpoint: string = 'https://www.nps.gov/nps-alerts.json?dt=159075838698';
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps() {
   const data = await axios.get(endpoint);
   return {
     props: {
