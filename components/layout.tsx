f29fe7213588b10f3529521d2c334376ee532ab3
SHewitt95 <hewitt.sherman@gmail.com>
2020-05-31 16:00:55 -0400

    add Head and Footer

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,16 @@
 import React from "react";
+import Head from "next/head";
 import styles from "./layout.module.scss";
 
 export default (props: { children: React.ReactNode }) => (
-  <main className={styles.container}>{props.children}</main>
+  <>
+    <Head>
+      <title>U.S. National Park - Park Status</title>
+    </Head>
+    <main className={styles.container}>{props.children}</main>
+    <footer>
+      <p>Created by Sherman Hewitt</p>
+      <p>Data comes from the U.S. National Park Service</p>
+    </footer>
+  </>
 );
