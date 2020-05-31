b0818d05770444bf14756403d91acdfaa6c8bdfd
SHewitt95 <hewitt.sherman@gmail.com>
2020-05-31 09:24:58 -0400

    use ParkPage

--- pages/park/[site_code].tsx
+++ pages/park/[site_code].tsx
@@ -1,63 +1,10 @@
 import axios from "axios";
 import { GetServerSideProps } from "next";
-import Link from "next/link";
-import ErrorPage from "next/error";
 import { formatData } from "../../helpers";
-import { FormattedItem, Status, ParkPageProps } from "../../types";
-import { Layout } from "../../components";
+import { ParkPageProps } from "../../types";
+import { ParkPage } from "../../components";
 
-const CATEGORIES: { [index: string]: string } = {
-  1: "Danger",
-  2: "Closure",
-  3: "Caution",
-  4: "Information",
-};
-
-const getUrl = (url: string): string => {
-  const root = "https://www.nps.gov";
-  if (url.includes(root)) {
-    return url;
-  } else {
-    return `${root}${url}`;
-  }
-};
-
-export default ({ parkData, err }: ParkPageProps) => {
-  if (err) return <ErrorPage statusCode={err.status} />;
-
-  const { park_name, state_name, statuses }: FormattedItem = parkData;
-  return (
-    <Layout>
-      <Link href="/">
-        <a>Go Back Home...</a>
-      </Link>
-
-      <h2>{park_name}</h2>
-      <p>State: {state_name}</p>
-      {Object.entries(statuses).map(([categoryID, statusArray], idx) => {
-        if (statusArray.length === 0) return null;
-        return (
-          <div key={idx}>
-            <h3>{CATEGORIES[categoryID]}</h3>
-            <ul>
-              {statusArray.map((status: Status, idx2: number) => (
-                <li key={idx2}>
-                  {status.title && <h4>{status.title}</h4>}
-                  {status.description && <p>{status.description}</p>}
-                  {status.internal_link && (
-                    <a target="_blank" href={getUrl(status.internal_link)}>
-                      More Information
-                    </a>
-                  )}
-                </li>
-              ))}
-            </ul>
-          </div>
-        );
-      })}
-    </Layout>
-  );
-};
+export default (props: ParkPageProps) => <ParkPage {...props} />;
 
 const endpoint: string = "https://www.nps.gov/nps-alerts.json";
 
